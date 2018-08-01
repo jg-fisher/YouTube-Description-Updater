5c999963dfb4defbeef238ef52483c537cf3e0b1
oalsing <oscar.alsing@gmail.com>
2018-08-01 11:10:42 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem "yt", "~> 0.32.2"
+
+gem "dotenv", "~> 2.5"
---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'yt'
+require 'dotenv/load'
+
+Yt.configure do |config|
+  config.api_key = ENV["api_key"]
+  config.client_id = ENV["client_id"]
+  config.client_secret = ENV["client_secret"]
+end
+
+def generate_new_description(description)
+  divider = "PEACE!"
+  if description.include? divider
+    matches = description.split(divider)
+    new_desc = matches[0] + divider + File.read("info")
+    new_desc
+  else
+    puts "Description did not contain: #{divider}"
+    description
+  end
+end
+
+channel = Yt::Channel.new id: ENV["channel_id"]
+account = Yt::Account.new refresh_token: ENV["refresh_token"]
+videos = channel.videos
+
+videos.each do |collection_video|
+  video = Yt::Video.new id: collection_video.id, auth: account
+  puts "Processing: #{video.title}"
+  new_description = generate_new_description(video.description)
+  video.update description: new_description
+end
+
